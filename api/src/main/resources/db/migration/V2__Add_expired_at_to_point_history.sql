a76bd025e40be58ec6437af8d4e0b95f82c6ff2c
sukkyun2 <goobghd@gmail.com>
2026-02-15 17:56:51 +0900

    chore(backend) flyway 싱크 문제 해결
    - public 스키마 고정
    - 불가피하게 sql 수정

--- api/src/main/resources/db/migration/V2__Add_expired_at_to_point_history.sql
+++ api/src/main/resources/db/migration/V2__Add_expired_at_to_point_history.sql
@@ -1,4 +1,4 @@
 -- 포인트 이력 테이블에 만료일시 컬럼 추가
-ALTER TABLE point_history ADD COLUMN expired_at TIMESTAMP;
+ALTER TABLE point_history ADD COLUMN IF NOT EXISTS expired_at TIMESTAMP;
 
 COMMENT ON COLUMN point_history.expired_at IS '포인트 만료 일시 (적립된 포인트의 경우만 해당)';
